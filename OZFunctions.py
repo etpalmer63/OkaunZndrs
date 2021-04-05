0bf38dab4757112e16b06c341e935747094519b6
Erik <ErikT63@gmail.com>
2021-04-05 12:08:22 -0400

    gui connected to functions from backend code

---
+++ OZFunctions.py
@@ -0,0 +1,225 @@
+"""
+This will be the collection of functions called by the GUI
+"""
+
+import random
+from copy import deepcopy
+
+# random.randint(0,9) - generates random int between 0,9 - time seeded
+
+
+
+def win_flip(single_flip,board):
+    if (single_flip == True):
+        if board["zndrs"]:      #Zndrsplt on battlefield
+            print("Draw a card")
+            print("*FLASHING LIGHTS*") 
+
+        if board["okaun"]:
+            board["okaun_pt"][0] = board["okaun_pt"][0] * 2
+            board["okaun_pt"][1] = board["okaun_pt"][1] * 2
+            print( "Okaun's new power and toughness are " + \
+                    str(board["okaun_pt"][0]) + "/" + \
+                    str(board["okaun_pt"][1]) + ".");
+
+            
+
+def flip_coin(single_flip, board):
+    print("Choose heads(0) or tails(1)")
+    hot = input(); 
+
+    if board["karak"]:   #karak's thumb on battlefield
+        print("Flipping two coins")
+        if (random.randint(0,1) == int(hot) or \
+           (random.randint(0,1) == int(hot))):
+            print("WIN")
+            win_flip(single_flip,board)
+            return True
+        else:
+            print("LOSE")
+            return False
+    else: 
+        print("Flipping one coin");
+        if (random.randint(0,1) == int(hot)):
+            print("WIN")
+            win_flip(single_flip,board)
+            return True
+        else:
+            print("LOSE")
+            return False
+        
+
+
+"""
+    choose a side, flip a coin. Continue until you loose. 
+    for each won flip, Okaun's power and toughness * 2^(won flips)
+    for each won flip, draw a card
+"""
+def flip_sequence(flips_won, okaun_pt):
+    
+    single_flip = False
+
+    while flip_coin(single_flip, okaun_pt):
+        print("You won a flip!")
+        flips_won += 1
+    return flips_won
+
+
+
+def precombat_flips(board):
+    flips_won = 0
+
+    if board["zndrs"]:
+        flips_won = flip_sequence(flips_won, board)
+    if board["okaun"]:
+        flips_won = flip_sequence(flips_won, board)
+
+    print( str(flips_won) + " flips won!")
+
+    if board["zndrs"]:
+        print( "Draw " + str(flips_won) + " cards")
+
+    if board["okaun"]:
+        board["okaun_pt"][0] = board["okaun_pt"][0] * 2**flips_won
+        board["okaun_pt"][1] = board["okaun_pt"][1] * 2**flips_won
+        print( "Okaun's new power and toughness are " \
+                + str(board["okaun_pt"][0]) + "/" \
+                + str(board["okaun_pt"][1]) + ".");
+
+        
+        
+def show_menu():
+    print("* ==== MENU ==== ")
+    print("* 1. Enter Okaun's Base Power/Toughness ")
+    print("* 2. Check Board Status")
+    print("* 3. Okaun Enter or Exit the Battlefield")
+    print("* 4. Zndrsplt Enter or Exit the Battlefield")
+    print("* 5. Karak's Thumb Enter or Exit the Battlefield")
+    print("* 6. Single Coin Flip")
+    print("* 7. Precombat Flip Sequence")
+    print("* 8. New Turn")
+    print("* 0. Exit")
+    print("* ============== ")
+
+   
+
+
+def check_board_status(board):
+    print(" === CHECKING BOARD STATUS === ")
+    if board["okaun"]:
+        print("Okaun is on the battlefield.")
+        print("Okaun is [" + str(board["okaun_pt"][0]) + "/" \
+                           + str(board["okaun_pt"][1]) + "]")
+    else:
+        print("Okaun is not on the battlefield.")
+            
+    if board["zndrs"]:
+        print("Zndrsplt is on the battlefield.")
+    else:
+        print("Zndrsplt is not on the battlefield.")
+
+    if board["karak"]:
+        print("Karak's Thumb is on the battlefield.")
+    else: 
+        print("Karak's Thumb not is on the battlefield.")
+
+
+
+def update_okaun_pt(board):
+    print("Enter Okaun's power")
+    board["okaun_pt_set"][0] = int(input())
+    print("Enter Okaun's toughness")
+    board["okaun_pt_set"][1] = int(input())
+
+    # this copies the base p/t to the current p/t
+    # using other methods caused updates to current p/t to change base p/t
+    board.update({"okaun_pt":deepcopy(board["okaun_pt_set"])})
+
+
+
+def toggle_okaun_otb(board):
+    if (board["okaun"]==False):
+        board["okaun"] = True  #Okaun on the battlefield
+        print("Okaun entered the battlefield.")
+        print("*FLASHING RED LIGHTS*")
+    else:
+        board["okaun"] = False  #Okaun not on the battlefield
+        print("Okaun exited the battlefield.")
+
+
+
+
+def toggle_zndrs_otb(board):
+    if (board["zndrs"]==False):
+        board["zndrs"] = True  #Zndrsplt on the battlefield
+        print("Zndrslpt entered the battlefield.")
+        print("*FLASHING BLUE LIGHTS*")
+        flash_blue_lights()
+    else:
+        board["zndrs"] = False  #Zndrsplt not on the battlefield
+        print("Zndrslpt exited the battlefield.")
+
+
+def toggle_karak_otb(board):
+    if (board["karak"]==False):
+        board["karak"] = True  #karak's thumb on the battlefield
+        print("Karak's Thumb entered the battlefield.")
+    else:
+        board["karak"] = False  #Karak's Thumb not on the battlefield
+        print("Karak's Thumb exited the battlefield.")
+
+
+def new_turn_reset(board):
+    # New turn -- reset okaun power and toughness
+    print("New turn, reseting Okaun's power and toughness.")
+
+    # this copies the base p/t to the current p/t
+    # using other methods caused updates to current p/t to change base p/t
+    board.update({"okaun_pt":deepcopy(board["okaun_pt_set"])})
+
+
+
+
+def menu_select(board):
+    menu_sel = int(input())
+
+    if(menu_sel==1):
+        update_okaun_pt(board)
+        return 1 
+    elif(menu_sel==2):
+        check_board_status(board)
+        return 2
+    elif(menu_sel==3):
+        toggle_okaun_otb(board)
+        return 3
+    elif(menu_sel==4):
+        toggle_zndrs_otb(board)
+        return 4
+    elif(menu_sel==5):
+        toggle_karak_otb(board)
+        return 5
+    elif(menu_sel==6):
+        flip_coin(True,board)
+        return 6
+    elif(menu_sel==7):
+        precombat_flips(board) 
+        return 7
+    elif(menu_sel==8):
+        new_turn_reset(board)
+        return 8
+    elif(menu_sel<=0):
+        exit()
+    else:
+        return 0
+
+
+
+def flash_blue_lights():
+    #enter code to make blue lights flash
+    print("The blue lights flashed")
+
+
+def flash_red_lights():
+    #enter code to make red lights flash
+    print("The red lights flashed")
+
--- OZ_gui.py
+++ OZ_gui.py
@@ -5,6 +5,11 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
 
+from OZFunctions import *
+import random
+from copy import deepcopy
+
+
 
 class menu_window(Gtk.Window):
     def __init__(self):
@@ -80,34 +85,67 @@ class menu_window(Gtk.Window):
 
     def on_button1_clicked(self, widget):
         print("Button 1 Clicked")
+        update_okaun_pt(board)
         var1 = menu_window()
         var1.show()
 
     def on_button2_clicked(self, widget):
+        check_board_status(board)
         print("Button 2 Clicked")
 
     def on_button3_clicked(self, widget):
+        toggle_okaun_otb(board)
         print("Button 3 Clicked")
 
     def on_button4_clicked(self, widget):
+        toggle_zndrs_otb(board)
         print("Button 4 Clicked")
 
     def on_button5_clicked(self, widget):
+        toggle_karak_otb(board)
         print("Button 5 Clicked")
 
     def on_button6_clicked(self, widget):
+        flip_coin(True,board) #single flip
         print("Button 6 Clicked")
 
     def on_button7_clicked(self, widget):
+        precombat_flips(board)
         print("Button 7 Clicked")
 
     def on_button8_clicked(self, widget):
+        new_turn_reset(board)
         print("Button 8 Clicked")
 
     #Quit button
     def on_button9_clicked(self, widget):
         print("Exit")
         Gtk.main_quit()
+
+
+
+
+
+
+
+
+
+
+
+"""
+This board dictionary contains inforamtion about the board state
+"""
+
+board = {
+    "okaun_pt" : [3,3],     # Okaun's current power and toughness
+    "okaun_pt_set": [3,3],  # Okaun's reset to power and toughness  
+    "okaun" : False,        # Okaun not on the battlefield to start
+    "zndrs" : False,        # Zndrsplt not on the battlefield to start  
+    "karak" : False         # Karak's Thumb not on the battlefield to start
+}
+
+
+
 
 win = menu_window()
 #win.fullscreen() #will cause the entire screen to fill
